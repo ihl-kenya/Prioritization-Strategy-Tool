de9cb2ec5664e472377a4f0be5d7aaf8636aa805
George Mwangi <mwangigeorge648@gmail.com>
2024-07-16 12:56:33 +0300

    database migrations

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,5 @@ from .base import Base, SessionLocal
 from .allocation import Allocations
 from .facility import Facility
 from .forecast import Forecast
-from .product import Product
+# from .product import Product
 from .user import User
--- models/product.py
+++ models/product.py
@@ -1,15 +1,16 @@
-from sqlalchemy import Column, String, Integer, Boolean
-from .base import Base
+# from sqlalchemy import Column, String, Integer, Boolean
+# from .base import Base
 
 
-class Product(Base):
-    __tablename__ = "products"
+# class Product(Base):
+#     __tablename__ = "products"
 
-    product_id = Column(Integer, primary_key=True,
-                        autoincrement=True, index=True)
-    product_name = Column(String, index=True, nullable=False)
-    product_category = Column(String, index=True, nullable=False)
-    is_equipment = Column(Boolean)
-    kmfl_classification = Column(String)
-    is_rmnch = Column(Boolean)
-    is_in_647_tracer_list = Column(Boolean)
+#     key = Column(Integer, primary_key=True,
+#                  autoincrement=True, index=True)
+#     product_name = Column(String, index=True, nullable=False)
+#     product_category = Column(String, index=True, nullable=False)
+#     is_equipment = Column(Boolean)
+#     kmfl_classification = Column(String)
+#     is_rmnch = Column(Boolean)
+#     is_in_647_tracer_list = Column(Boolean)
+#     product_id = Column(String, nullable=False)
