ddeae5c4e8f7e89f56e0d7bf88ec930d400a6239
George Mwangi <mwangigeorge648@gmail.com>
2024-07-17 17:02:40 +0300

    Updated products table model

--- models/product.py
+++ models/product.py
@@ -1,16 +1,15 @@
-# from sqlalchemy import Column, String, Integer, Boolean
-# from .base import Base
+from sqlalchemy import Column, String, Integer, Boolean, Float
+from .base import Base
 
 
-# class Product(Base):
-#     __tablename__ = "products"
+class Product(Base):
+    __tablename__ = "products"
 
-#     key = Column(Integer, primary_key=True,
-#                  autoincrement=True, index=True)
-#     product_name = Column(String, index=True, nullable=False)
-#     product_category = Column(String, index=True, nullable=False)
-#     is_equipment = Column(Boolean)
-#     kmfl_classification = Column(String)
-#     is_rmnch = Column(Boolean)
-#     is_in_647_tracer_list = Column(Boolean)
-#     product_id = Column(String, nullable=False)
+    product_id = Column(Integer, nullable=False)
+    product_name = Column(String, index=True, nullable=False)
+    product_category = Column(String, index=True, nullable=False)
+    is_equipment = Column(Boolean)
+    kmfl_classification = Column(String)
+    is_rmnch = Column(Boolean)
+    is_in_647_tracer_list = Column(Boolean)
+    avg_price_kes = Column(Float)
