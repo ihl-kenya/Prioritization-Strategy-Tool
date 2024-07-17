31a8094a7e8961a012e740c4c13e955b68ef65a5
George Mwangi <mwangigeorge648@gmail.com>
2024-07-17 23:13:22 +0300

    Migrated model changes to DB

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 from .base import Base, SessionLocal
 from .facilities import Facility
 from .forecasts import Forecast
+from .organizations import Organizations
 from .products import Product
 from .users import User
