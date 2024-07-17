1b1c6b3912f66c9053eddeb1b6670216812ab2ab
George Mwangi <mwangigeorge648@gmail.com>
2024-07-17 22:40:24 +0300

    Modified forecasts and facilities table models

--- models/forecasts.py
+++ models/forecasts.py
@@ -1,25 +1,16 @@
-from sqlalchemy import Column, String, Integer, Boolean, Float
+from sqlalchemy import Column, String, Integer, Boolean, Float, ForeignKey
 from .base import Base
 
 
 class Forecast(Base):
     __tablename__ = "forecasts"
 
-    key = Column(Integer, primary_key=True, autoincrement=True)
-    county = Column(String, nullable=False, index=True)
-    sub_county = Column(String, nullable=False, index=True)
-    facility_name = Column(String, nullable=False, index=True)
-    keph_level = Column(String, nullable=False, index=True)
-    product_category = Column(String, nullable=False, index=True)
-    ven = Column(String, index=True)
-    funding = Column(String, index=True)
-    product_name = Column(String, nullable=False)
-    pack_size = Column(String)
-    price_kes = Column(Float)
+    forecast_id = Column(Integer, primary_key=True, autoincrement=True)
+    facility_id = Column(Integer, ForeignKey("facilities.facility_id"))
     quantity_required_for_one_year = Column(Float)
+    product_id = Column(Integer, ForeignKey("products.product_id"))
+    ven = Column(String, index=True)
+    price_kes = Column(Float)
     value_of_quantities_required_for_12_months = Column(Float)
-    is_equipment = Column(Boolean, index=True)
-    kmfl_classification = Column(String, index=True)
-    is_rmnch = Column(Boolean, index=True)
-    is_in_647_tracer_list = Column(Boolean, index=True)
-    product_id = Column(String, nullable=False, index=True)
+    funding = Column(String, index=True)
+    pack_size = Column(String)
